f7e334e06b17a3265d3bf320a6ebee66c49ff88f
Yusuf260511 <sultvnhuzaevusufhuza@gmail.com>
2024-12-16 11:15:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let random = Math.floor(Math.random()*100)
+let randomMulti = random*2
+let randomDivi = random/2
+let randomRema = random%3
+
+alert('Sizda hozir random sonlar chiqishni boshlaydi😉')
+alert(`Random orqali chiqqan son: ${random}`)
+alert("Biz random sonni 2 ga ko'paytirmoqchimiz va bo'lmoqchimiz!")
+alert(`Random orqali chiqqan son: ${random} edi \nBiz uni 2 ga ko'paytirib: ${randomMulti} sonini chiqardik, \nVa shu sonni 2 ga bo'lib: ${randomDivi} sonini chiqardik, \nVa shu sonni 3 ga bo'lgandagi qoldiq: ${randomRema} sonini chiqardik`)
